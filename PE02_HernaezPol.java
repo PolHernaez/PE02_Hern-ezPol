583e5a5314cdac5710c6e36417db0b4c858e4e3c
Pol Hernáez Salvà <alu.pol.hernaez@mataro.epiaedu.cat>
2025-10-05 21:14:05 +0200

    4t Commit

--- PE02_HernaezPol.java
+++ PE02_HernaezPol.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 class jocfutbol {
     public static void main(String[] args) {
     Scanner primer = new Scanner (System.in);
-    String protestar;
-    String passarPilota, passarPilota2;
-    String falta, falta2;
-    int golLocal;
-    int golRival;
-    String guanyatPerdut;
-    String porter, xutar;
-    boolean groga;
+    String passarPilota, passarPilota2, protestar, falta, falta2;
+    int golLocal, golRival;
+    String porter, xutar, guanyatPerdut;
+    boolean groga, confiança, companyerisme;
+    companyerisme = false;
+    confiança = false;
     golRival = 1;
     golLocal = 1;
     System.out.println("Estàs jugant un partit de futbol vas empat 1-1, queden 6 minuts i et fan una falta amb una entrada forta i l'àrbitre no pita falta, vols portestar? (si/no)");
@@ -21,40 +19,49 @@ class jocfutbol {
     System.out.println("El partit segueix i ara aneu perdent 1-2.");
     golRival = (golRival + 1);
     System.out.println("Tens un contraatac");
-    System.out.println("A que vols fer amb la pilota?");
-    System.out.println("A) Passar-la al jugador de la dreta");
-    System.out.println("B) Passar-la al jugador de l'esquerra");
+    System.out.println("Que vols fer amb la pilota?");
+    System.out.println("A) Passar-la al jugador de la dreta (+1 punt de companyerisme)");
+    System.out.println("B) Passar-la al jugador de l'esquerra (+1 punt de companyerisme)");
     System.out.println("C) Xutar des de fora l'àrea");
     System.out.println("(a/b/c)");
     passarPilota = primer.nextLine();
 
     if (passarPilota.equals ("a")){
         System.out.println("Pases la pilota al jugador de la dreta.");
-        System.out.println("El teu company corre, xuta i fa gol!");
-        golLocal = (golLocal + 1);
+        System.out.println("El teu company corre, xuta i la para el porter");
+        System.out.println("El marcador segueix " + golLocal + "-" + golRival);
+        companyerisme = true;
     }
     else if (passarPilota.equals("b")) {
             System.out.println("Pases la pilota al jugador de l'esquerra.");
             System.out.println("El teu company controla, centra a l'àrea...");
-            System.out.println("La pilota impacte en un rival i... GOOOOL en pròpia porta del rival!");
+            System.out.println("La pilota impacta en un rival i... GOOOOL en pròpia porta del rival!");
             golLocal = (golLocal + 1);
+            System.out.println("Marcador: " + golLocal + "-" + golRival);
+            companyerisme = true;
     }
     else if (passarPilota.equals("c")) {
             System.out.println("Decideixes xutar des de fora l'àrea.");
-            System.out.println("Rellisques i la pilota se'n va per damunt de la porteria.");
+            System.out.println("Fas un GOLÀS per l'escaire");
+            golLocal = (golLocal + 1);
+            System.out.println("Marcador: " + golLocal + "-" + golRival);
+
+            
     }
     else {
         System.out.println("Resposta no vàlida");
         System.exit(0);
     }
 
-    System.out.println("Queda un minut de partit.");
+    System.out.println("Queden tres minuts de partit.");
         System.out.println("Els rivals fan una bona jugada i el jugador que està al teu davant té la pilota.");
         System.out.println("Què fas?");
         System.out.println("A) Fer falta");
         System.out.println("B) No fer falta");
         System.out.print("(a/b): ");
         falta = primer.nextLine();
+        
+
         if (falta.equals("a")) {
             System.out.println("Fas falta al rival i l'arbitre et mostra targeta groga");
             if (groga) {
@@ -62,13 +69,23 @@ class jocfutbol {
                             
             } 
             else {
-                System.out.println("Com que es la primera targeta no passa res");
+                System.out.println("Com que és la primera targeta, no passa res");
             }
         } else if (falta.equals("b")) {
             System.out.println("Decideixes no fer falta.");
             System.out.println("El davanter rival xuta i la pilota impacta al pal!");
             System.out.println("El lateral del teu equip allunya la pilota.");
-            System.out.println("Quan entres a l'àrea rival et fan penal!");
+            System.out.println("Ets sol al segon pal, només cal que el teu company te la passi… gol segur! (requereix 1 punt de companyerisme)");
+            if (companyerisme){
+            System.out.println("El teu company et la passa i GOOOOOL");
+            golLocal = (golLocal + 1);
+            System.out.println("Marcador: " + golLocal + "-" + golRival);
+            }
+            else {
+            System.out.println("El teu company decideix no passar la pilota i la perd");
+            }
+            System.out.println("Queda un minut de partit.");
+            System.out.println("Entres a l'àrea rival amb la pilota controlada i et fan penal!");
             System.out.println("Com xutes el penal?");
             System.out.println("A) Xutar al mig");
             System.out.println("B) Xutar a la dreta");
@@ -78,11 +95,14 @@ class jocfutbol {
             if (penal.equals("a")) {
                 System.out.println("Xutes al mig i el porter no es mou. GOOOL!");
                 golLocal = (golLocal + 1);
+                
             } else if (penal.equals("b")) {
                 System.out.println("Xutes a la dreta i el porter l'atura.");
+               
             } else if (penal.equals("c")) {
                 System.out.println("Xutes a l'esquerra i marques un gran gol!");
                 golLocal = (golLocal + 1);
+                
             } else {
                 System.out.println("Opció no vàlida.");
             }
@@ -106,15 +126,20 @@ if (porter.equals ("a")) {
     System.out.println("Li dius al porter que es llenci a la dreta.");
     System.out.println("El xutador xuta al mig i marca gol pels rivals!");
     golRival = (golRival + 1);
+    System.out.println("Marcador: " + golLocal + "-" + golRival);
 }
 else if (porter.equals ("b")) {
     System.out.println("Li dius al porter que es quedi al mig.");
-    System.out.println("El xutador xuta al mig i el porter atura la pilota!");
+    System.out.println("El xutador xuta al mig i el porter atura la pilota! (guanyes un punt de confiança)");
+    System.out.println("El marcador segueix " + golLocal + "-" + golRival);
+    confiança = true;
 }
 else if (porter.equals ("c")) {
     System.out.println("Li dius al porter que es llenci a l'esquerra.");
     System.out.println("El xutador xuta al mig i marca gol pels rivals!");
     golRival = (golRival + 1);
+    System.out.println("Marcador: " + golLocal + "-" + golRival);
+
 }
 System.out.println("El partit segueix i queda un minut.");
 System.out.println("Els rivals fan una bona jugada i el jugador que està al teu davant té la pilota.");
@@ -129,27 +154,50 @@ if (falta2.equals("a")){
     System.out.println("Els rivals treuen ràpid, però s'equivoquen i robes la pilota.");
     System.out.println("Et quedes sol contra el porter que s'avança cap a tu.");
     System.out.println("Com xutes?");
-    System.out.println("A) Intentar picar la pilota per sobre del porter");
+    System.out.println("A) Intentar picar la pilota per sobre del porter, si ho fas bé és gol segur (requereix 1 punt de confiança)");
     System.out.println("B) Xutar rasa al primer pal");
     System.out.println("C) Xutar entre les cames del porter");
     System.out.print("(a/b/c): ");
     xutar = primer.nextLine();
     if (xutar.equals("a")){
+        if (confiança) {
         System.out.println("Intentes picar la pilota per sobre del porter.");
         System.out.println("El porter no s'ho espera i marques gol!");
         golLocal = (golLocal + 1);
+       
+        }
+        else {
+        System.out.println("No disposes de punts de confiança");
+         System.out.println("B) Xutar rasa al primer pal");
+        System.out.println("C) Xutar entre les cames del porter");
+        xutar = primer.nextLine();
+        }
+        if (xutar.equals("b")){
+        System.out.println("Xutes rasa al primer pal.");
+        System.out.println("El porter fa una estirada increïble i atura la pilota.");
+       
+        }
+        else if (xutar.equals("c")) {
+        System.out.println("Xutes entre les cames del porter.");
+        System.out.println("El sorprens i la pilota li passa per sota... GOL!");
+        golLocal = (golLocal + 1);
         System.out.println("El partit s'acaba.");
+         }
+        else {
+            System.out.println("Resposta no vàlida");
+            System.exit(0);
+        }
     }
     else if (xutar.equals("b")){
         System.out.println("Xutes rasa al primer pal.");
         System.out.println("El porter fa una estirada increïble i atura la pilota.");
-        System.out.println("El partit s'acaba.");
+      
     }
     else if (xutar.equals("c")) {
         System.out.println("Xutes entre les cames del porter.");
         System.out.println("El sorprens i la pilota li passa per sota... GOL!");
         golLocal = (golLocal + 1);
-        System.out.println("El partit s'acaba.");
+    
     }
     else {
             System.out.println("Resposta no vàlida");
@@ -164,30 +212,51 @@ if (falta2.equals("a")){
         System.out.println("Què fas?");
         System.out.println("A) Passar-la al jugador de la dreta");
         System.out.println("B) Passar-la al jugador de l'esquerra");
-        System.out.println("C) Xutar des de fora l'àrea");
+        System.out.println("C) Xutar des de fora de l'àrea (requereix un punt de confiança)");
         System.out.print("(a/b/c): ");
         passarPilota2 = primer.nextLine();
         if (passarPilota2.equals("a")){
             System.out.println("Pases al jugador de la dreta.");
             System.out.println("El teu company corre, però el defensor encara més i li pren la pilota.");
-            System.out.println("El partit s'acaba.");
+            
         }
         else if (passarPilota2.equals("b")){
             System.out.println("Pases al jugador de l'esquerra.");
             System.out.println("El teu company et torna la pilota i al primer toc marques un golàs!");
             golLocal = (golLocal + 1);
-            System.out.println("El partit s'acaba.");
+            
         }
         else if (passarPilota2.equals("c")) {
-            System.out.println("Decideixes xutar des de fora l'àrea.");
+            if (confiança) {
+            System.out.println("Decideixes xutar des de fora de l'àrea.");
             System.out.println("La pilota impacta al travesser, rebota a l'esquena del porter i entra!");
             golLocal = (golLocal + 1);
-            System.out.println("GOOOL! El partit s'acaba.");
+            System.out.println("GOOOL!");
+            }
+            else  {
+            System.out.println("No disposes de punts de confiança");
+            System.out.println("A) Passar-la al jugador de la dreta");
+            System.out.println("B) Passar-la al jugador de l'esquerra");
+            passarPilota2 =primer.nextLine();
+            if (passarPilota2.equals("a")){
+            System.out.println("Pases al jugador de la dreta.");
+            System.out.println("El teu company corre, però el defensor encara més i li pren la pilota.");
+            
         }
-        else {
+            else if (passarPilota2.equals("b")){
+            System.out.println("Pases al jugador de l'esquerra.");
+            System.out.println("El teu company et torna la pilota i al primer toc marques un golàs!");
+            golLocal = (golLocal + 1);
+            
+            }
+            else {
             System.out.println("Resposta no vàlida");
             System.exit(0);
+            
         }
+        }
+    }
+            
     }
 
 } else {
